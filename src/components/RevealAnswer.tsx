17005e7dc2bf7a1ccfbd141ba27e3eff57052721
Benjamin Zifcak <bmzifcak@gmail.com>
2026-03-09 17:33:48 -0400

    fixed linting errors

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -5,6 +5,6 @@ export function RevealAnswer(): React.JSX.Element {
     const [isVisible, setVisible] = useState<boolean>(false);
     return <>
     <div>Reveal Answer: {isVisible && <span>42</span>}</div>
-    <Button onClick={()=>{setVisible(!isVisible); console.log(isVisible)}}>Reveal</Button>
+    <Button onClick={()=>{setVisible(!isVisible); console.log(isVisible)}}>Reveal Answer</Button>
     </>
 }
