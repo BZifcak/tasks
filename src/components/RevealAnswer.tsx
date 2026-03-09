106f40d13f00f12cd030b2557e43b7a2afe4cd71
Benjamin Zifcak <bmzifcak@gmail.com>
2026-03-09 15:17:05 -0400

    Implemented counter and revealAnswer components

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,9 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): React.JSX.Element {
-    return <div>Reveal Answer</div>;
+    const [isVisible, setVisible] = useState<boolean>(false);
+    return <>
+    <div>Reveal Answer: {isVisible && <span>42</span>}</div>
+    <Button onClick={()=>{setVisible(!isVisible); console.log(isVisible)}}>Reveal</Button>
+    </>
 }
